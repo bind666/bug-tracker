c2fb7ad0e893b8277186dab834c0c5cb64a88e37
bind666 <144540630+bind666@users.noreply.github.com>
2025-06-19 18:04:33 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "1MB", extended: true }));
 app.use(cookieParser())
 // app.use(cors());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://bug-tracker-liard.vercel.app"],
     credentials: true,
 }))
 
